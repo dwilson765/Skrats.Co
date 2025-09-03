11a7c354e8b6f43250316fe83a99a094a65eb389
David Wilson <wilsondavid0611@gmail.com>
2025-09-03 00:13:30 -0700

    bunch of changes

--- app.js
+++ app.js
@@ -2,27 +2,14 @@ const app = Vue.createApp({
     data() {
         return {
             sections: [
-                { id: "skateboarding transition", title: "Skateboarding", description: "Discover the best skateboarding gear and tricks.", img: "skating.jpg", link: "Skateboarding.html" },
-                { id: "music", title: "Music", description: "Learn how to create your own music.", img: "skratBanner.png" },
-                { id: "art", title: "Art", description: "Improve your art skills with tutorials.", img: "SLUDGE_LIFE_2.jpeg" },
-                { id: "electronics", title: "Electronics", description: "Explore electronic projects.", img: "electronics.jpg" }
-
-            ],
-            sections2: [
-                { id: "Frankye", title: "Frankye", description: "Yap.", img: "images/GOPR1337.JPG" },
-                { id: "Arden", title: "Arden", description: "Yap.", img: "images/GOPR1354.JPG" },
-                { id: "Jon", title: "Jon", description: "Yap.", img: "images/G0061400.JPG" },
-                { id: "David", title: "David", description: "Yap.", img: "images/G0071409.JPG" }
+                { id: "skateboarding transition", title: "Skateboarding", description: "Skating is where most of this stemmed from. Take a look at what we've been up to!", img: "skating.jpg", link: "Skateboarding.html" },
+                { id: "music", title: "Music", description: "Take a listen", img: "skratBanner.png" },
+                { id: "art", title: "Art", description: "Take a look", img: "SLUDGE_LIFE_2.jpeg" },
+                { id: "electronics", title: "Electronics", description: "SOme more STEM-focused projects", img: "electronics.jpg" }
 
             ]
-        };
+        }
     }
 });
-app.mount("#app");
 
-
-$('.carousel').carousel({
-  interval: 4000,
-  ride: "carousel"
-})
-
+app.mount("#app")
---
+++ skate_app.js
@@ -0,0 +1,47 @@
+const app = Vue.createApp({
+    data() {
+        return {
+          sections2: [
+                { id: "Frankye", title: "Frankye", description: "Deck: 8.38” Welcome<br> Wheels: Spitfire Radial Fulls<br> Bearings: Bone Reds<br> Trucks: Thunder trucks", img: "images/GOPR1337.JPG" },
+                { id: "Arden", title: "Arden", description: "Deck: 8.75” Crooked<br> Wheels: Spitfire Lockins<br> Bearings: Bone Reds<br> Trucks: 159 Hollow Indie", img: "images/arden_skate.jpg" },
+                { id: "Jon", title: "Jon", description: "Yap.", img: "images/G0061400.JPG" },
+                { id: "David", title: "David", description: "Board: 8.125” Toy Machine<br> Wheels: Spitefire Forumla Fours<br> Bearings: Bone Reds<br> Trucks: Thunder Light Hollows", img: "images/G0071409.JPG" }
+
+            ],
+
+            collageColumns: 4, // Default number of columns
+            collageImages: [
+                "images/IMG_6868.jpg",
+                "images/IMG_5400.jpg",
+                "images/IMG_5281.jpg",
+                "images/IMG_8379.jpg",
+                "images/IMG_8388.jpg",
+                "images/standing.jpg",
+                
+                
+                // Add more image paths as needed
+            ]};
+    },          
+    computed: {
+        collageGrid() {
+        // Split images into columns for the grid
+        const cols = Array.from({ length: this.collageColumns }, () => []);
+        this.collageImages.forEach((img, i) => {
+            cols[i % this.collageColumns].push(img);
+        });
+        return cols;
+            }
+        }
+});
+
+// Carousel
+app.mount("#app");
+
+
+$('.carousel').carousel({
+  interval: 4000,
+  ride: "carousel"
+})
+
+// vue
+
